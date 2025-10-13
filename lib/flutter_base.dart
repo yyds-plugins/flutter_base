170304eee8c75751ae86f390cdf3ecadec80a7ce
X <135988607+yyds-x@users.noreply.github.com>
2025-10-13 10:42:13 +0800

    docs: 导出新增的logger_util工具类

--- lib/flutter_base.dart
+++ lib/flutter_base.dart
@@ -7,6 +7,7 @@ export 'package:flutter_base/widget/update_dialog/update_dialog.dart';
 export 'package:flutter_base/utils/value_util.dart';
 export 'package:flutter_base/utils/color_utils.dart';
 export 'package:flutter_base/utils/time_utils.dart';
+export 'package:flutter_base/utils/logger_util.dart';
 
 export 'package:flutter_base/widget/widget.dart';
 export 'package:flutter_base/widget/tab_bar_indicator.dart';
